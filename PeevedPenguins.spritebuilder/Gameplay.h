dbec7ec946de6dbea824ba04d5d480eeaf26e03c
judy mckelvey <judy@judys-MacBook.local>
2014-07-13 12:35:24 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by judy mckelvey on 7/13/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
